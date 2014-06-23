b63f0a5b830d13f35a68e8af280a0441d51146a5
James Zapico <james.zapico@gmail.com>
2014-06-23 00:15:17 -0500

    Should be GPL v2 to comply with the borrowed code.

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,9 +1,10 @@
 /**
- * Copyright (c) 2014 James Zapico <james.zapico@gmail.com>
+ * This example program demonstrates the use of the BBB-eQEP library.
+ * Copyright (C) 2014 James Zapico <james.zapico@gmail.com>
  * 
- * This program is free software: you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
--- src/bbb-eqep.cpp
+++ src/bbb-eqep.cpp
@@ -1,9 +1,10 @@
 /**
- * Copyright (c) 2014 James Zapico <james.zapico@gmail.com>
+ * This library provides fast access to the Beaglebone Black's onboard eQEP modules.
+ * Copyright (C) 2014 James Zapico <james.zapico@gmail.com>
  * 
- * This program is free software: you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
